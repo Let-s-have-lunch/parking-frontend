e62568f998db3dacdf9bf02f782a625d823fcdd3
DeveloperSky95 <haneulgim95@gmail.com>
2026-08-31 11:48:34 +0900

    app/_layout.tsx, app/index.tsx 작성

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,19 @@
 import "../styles/global.css";
+
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { Slot } from "expo-router";
 
+SplashScreen.preventAutoHideAsync().then(() => {});
+
 export default function RootLayout() {
-    return <Slot />;
+    return (
+        <SafeAreaProvider>
+            <StatusBar style={"light"} />
+            <SafeAreaView className={"flex-1"}>
+                <Slot />
+            </SafeAreaView>
+        </SafeAreaProvider>
+    );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
-import { Text } from "react-native";
+import { ActivityIndicator, View } from "react-native";
 
-function HomeScreen() {
+function RootPage() {
     return (
-        <Text>Initialization</Text>
-    )
+        <View className="flex-1 items-center justify-center bg-white">
+            <ActivityIndicator size="large" color="#2288ED" />
+        </View>
+    );
 }
 
-export default HomeScreen;
+export default RootPage;
