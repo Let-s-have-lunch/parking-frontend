78934e38d33bac4f2f09ab9e2d4f647c482029dc
DeveloperSky95 <haneulgim95@gmail.com>
2026-09-02 14:30:43 +0900

    오류 수정

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,8 @@
 import "../styles/global.css";
 
-import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { Slot } from "expo-router";
-
-SplashScreen.preventAutoHideAsync().then(() => {});
 
 export default function RootLayout() {
     return (
